9771170a21553bcaff387fe3df46017d11532439
AlmightyVova <vovkolimur@ukr.net>
2021-03-31 23:29:14 +0300

    created basic keyboard 3rd person controller

---
+++ Assets/ThirdPersonMovement.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Transactions;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public float speed = 6f;
+    public Transform cam;
+
+    public float turnSmoothTime = 0.1f;
+    private float _turnSmoothVelocity;
+
+    private void Start()
+    {
+        controller.detectCollisions = false;
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity,
+                turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDirection.normalized * (speed * Time.deltaTime));
+        }
+    }
+}
